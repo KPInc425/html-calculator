e8bedcaab92112be609525bf6c164ac325ea7851
KPInc425 <KPInc425@gmail.com>
2021-07-14 17:38:30 -0700

    Changed some styling, added clear button, created operate function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,24 @@
+let calculatedNumber = 0;
+
+
+function operate(operator, numOne, numTwo) {
+    switch (operator) {
+        case ('+'):
+            calculatedNumber = add(numOne, numTwo);
+            break;
+        case ('-'):
+            calculatedNumber = subtract(numOne, numTwo);
+            break;
+        case ('*'):
+            calculatedNumber = multiply(numOne, numTwo);
+            break;
+        case ('/'):
+            calculatedNumber = divide(numOne, numTwo);
+            break;
+    }
+    console.log(calculatedNumber);
+}
+
 function add(...args) {
     let sum = 0;
     for (let arg of args) {
