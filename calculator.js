67c01af0d2f617e9e2c0542863a1812e006efe76
KPInc425 <KPInc425@gmail.com>
2021-07-14 20:12:51 -0700

    Began work on connecting logic to calculator (Input numbers and Length Check)

--- calculator.js
+++ calculator.js
@@ -1,22 +1,72 @@
-let calculatedNumber = 0;
+let CALCULATED_NUMBER = 0;
+let NUM_ARRAY_ONE = [];
+let NUM_ARRAY_TWO = [];
+let NUM_ONE = 0;
+let NUM_TWO = 0;
+let NUM_INDEX = 1;
 
+let calcDisplayText = document.querySelector('#calcDisplayText');
+
+btnNumPress();
+
+
+
+
+//Event listeners added to number buttons
+function btnNumPress() {
+    let btnArray = document.querySelectorAll('.btnNum');
+
+    btnArray.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            // alert(btn.value);
+            // A check to make sure number doesn't get too large for display
+            if (NUM_INDEX == 1) {
+                if (NUM_ARRAY_ONE.length >= 14) {
+                    alert("The Number you are attempting is too large.")
+                } else {
+                    NUM_ARRAY_ONE.push(btn.value);
+
+                    // We make array a string, remove all , to make string display like a number
+                    let newCalcDisplayText = NUM_ARRAY_ONE.toString().replace(/,/g, "");
+                    calcDisplayText.textContent = newCalcDisplayText;
+    
+                    // We store the string as a number for future computation
+                    NUM_ONE = Number(newCalcDisplayText);
+                }
+            } else {
+                if (NUM_ARRAY_ONE.length >= 14) {
+                    alert("The Number you are attempting is too large.")
+                } else {
+                    NUM_ARRAY_TWO.push(btn.value);
+
+                    let newCalcDisplayText = NUM_ARRAY_TWO.toString().replace(/,/g, ""); 
+                    calcDisplayText.textContent = newCalcDisplayText;
+                    
+                    NUM_TWO = Number(newCalcDisplayText);
+                }
+
+            }
+            // calcDisplayText.textContent = btn.value;
+        })
+    })
+}
 
 function operate(operator, numOne, numTwo) {
     switch (operator) {
         case ('+'):
-            calculatedNumber = add(numOne, numTwo);
+            CALCULATED_NUMBER = add(numOne, numTwo);
             break;
         case ('-'):
-            calculatedNumber = subtract(numOne, numTwo);
+            CALCULATED_NUMBER = subtract(numOne, numTwo);
             break;
         case ('*'):
-            calculatedNumber = multiply(numOne, numTwo);
+            CALCULATED_NUMBER = multiply(numOne, numTwo);
             break;
         case ('/'):
-            calculatedNumber = divide(numOne, numTwo);
+            CALCULATED_NUMBER = divide(numOne, numTwo);
             break;
     }
-    console.log(calculatedNumber);
+    console.log(CALCULATED_NUMBER);
 }
 
 function add(...args) {
